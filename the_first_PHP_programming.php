cd4cf66690d7574ea586afdd1d21f6e6b10fe434
Hong Cuc <nt.hongcuc03@gmail.com>
2023-11-05 11:06:29 +0700

    add another php file

---
+++ the_first_PHP_programming.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Program</title>
+</head>
+<body>
+    <h1>Sum of Two Numbers</h1>
+    
+    <?php
+    // Define two numbers
+    $num1 = 10;
+    $num2 = 5;
+
+    // Calculate the sum
+    $sum = $num1 + $num2;
+
+    // Display the result
+    echo "The sum of $num1 and $num2 is: $sum";
+    ?>
+</body>
+</html>
